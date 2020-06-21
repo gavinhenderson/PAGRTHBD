fa85edf550f6370caba4a48a5c9ed31c8df4ff88
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2020-06-21 12:02:04 +0100

    Added tests and basic implementation

--- src/loader/parse-exports.spec.ts
+++ src/loader/parse-exports.spec.ts
@@ -2,11 +2,36 @@ import { parseExports } from "./parse-exports";
 
 it("can parse single named export", () => {
   const INPUT = `
-        export const helloWorld = () => {}
+    export const inlineNamedExport = () => {}
     `;
 
   const exports = parseExports(INPUT);
 
   expect(exports.namedExports).toHaveLength(1);
-  expect(exports.namedExports[0]).toBe("helloWorld");
+  expect(exports.namedExports[0]).toBe("inlineNamedExport");
+});
+
+it("can parse single named export when its not exported inline", () => {
+  const INPUT = `
+    const nameExportNotInline = () => {};
+
+    export { nameExportNotInline };
+    `;
+
+  const exports = parseExports(INPUT);
+
+  expect(exports.namedExports).toHaveLength(1);
+  expect(exports.namedExports[0]).toBe("nameExportNotInline");
+});
+
+it("can parse named exports when they are comma seperated", () => {
+  const INPUT = `
+    export const namedExportOne = 1, namedExportTwo = 2;
+    `;
+
+  const exports = parseExports(INPUT);
+
+  expect(exports.namedExports).toHaveLength(2);
+  expect(exports.namedExports).toContain("namedExportOne");
+  expect(exports.namedExports).toContain("namedExportTwo");
 });
--- src/loader/parse-exports.ts
+++ src/loader/parse-exports.ts
@@ -1,3 +1,32 @@
-export const parseExports = (inputCode: String) => {
-  return { namedExports: ["helloWorld"] };
+import { parse } from "@babel/parser";
+
+export const parseExports = (inputCode: string) => {
+  // TODO The sourceType should be set by config somewhere
+  const babelResult = parse(inputCode, { sourceType: "module" });
+
+  const {
+    program: { body: programBody },
+  } = babelResult;
+
+  // TODO Make this not an any
+  const exportNameDeclarationNodes: any[] = programBody.filter(
+    ({ type }) => type === "ExportNamedDeclaration"
+  );
+
+  // TODO This super deep destructuring is probably pretty unsafe, although if we fix the types that might not matter
+  const namedExports = exportNameDeclarationNodes.map(
+    ({
+      declaration: {
+        declarations: [
+          {
+            id: { name },
+          },
+        ],
+      },
+    }: {
+      declaration: any;
+    }) => name
+  ) as any;
+
+  return { namedExports };
 };
